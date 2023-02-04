d8e57cd80132f9b8263076d1f5402597ba954b02
Jenny Yu <jenny100.yu@gmail.com>
2023-02-04 15:25:28 -0500

    Initialized node and express.
    Made app.js and package.json. Rearranged directory structure.

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+export const app = express();
+
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  console.log("HTTP request", req.method, req.url, req.body);
+  next();
+});
+
+const PORT = 3000;
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log("HTTP server on http://localhost:%s", PORT);
+});
